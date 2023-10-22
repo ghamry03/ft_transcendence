46f12d3b517a658aaea04ff6f4ac20cd204d3e88
coding <kareemamin2005@gmail.com>
2023-10-22 13:07:42 +0400

    adding frontend

---
+++ frontendd/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="n in 3" :key="n">
+        <v-btn>some TITLE</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  name: 'HelloWorld',
+})
+</script>
