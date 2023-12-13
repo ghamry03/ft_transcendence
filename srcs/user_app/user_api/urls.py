816fd51f123c43f8012a0aaf478cea55a05ee5d1
Omar <omarelghamry@icloud.com>
2023-12-13 18:31:47 +0400

    Added a user api endpoint to get indvidual users
    Added:
    - added a user api endpoint where it's going to use user id to do
      GET/PUT/DELETE requests
    
    TODO:
    - clean the class-based views
    - figure out the permissions for different users to do the call
    - document the api's different end points
    - check with the team for updates about the actual tables and modify the
      models to adapt to it

--- srcs/user_app/user_api/urls.py
+++ srcs/user_app/user_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
         UsersListApiView,
+        UserDetailApiView,
 )
 
 urlpatterns = [
     path('api/', UsersListApiView.as_view()),
+    path('api/<int:user_id>/', UserDetailApiView.as_view()),
 ]
--- srcs/user_app/user_api/views.py
+++ srcs/user_app/user_api/views.py
@@ -17,14 +17,72 @@ class UsersListApiView(APIView):
         return Response(serlializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
-        data = {
-            'id': request.data.get('id'),
-            'username': request.data.get('username'),
-            'first_name': request.data.get('first_name'),
-            'last_name': request.data.get('last_name'),
-        }
-        serializer = UserSerializer(data=data)
+        # data = {
+        #     'id': request.data.get('id'),
+        #     'username': request.data.get('username'),
+        #     'first_name': request.data.get('first_name'),
+        #     'last_name': request.data.get('last_name'),
+        # }
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailApiView(APIView):
+    # TODO: figure it oud
+    # permission_classes = [permissions.IsAuthenticated]
+
+    def getObjectById(self, user_id):
+        try:
+            return User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return None
+
+    def get(self, requset, user_id, *args, **kwargs):
+        user_query = self.getObjectById(user_id)
+        if user_query is None:
+            return Response(
+                {"res", "User with this id does not exist"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = UserSerializer(user_query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, user_id, *args, **kwargs):
+        user_query = self.getObjectById(user_id)
+        if not user_query:
+            return Response(
+                {"res", "User with this id does not exist"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        # reqData = {
+        #     'id': request.data.get('id'),
+        #     'username': request.data.get('username'),
+        #     'first_name': request.data.get('first_name'),
+        #     'last_name': request.data.get('last_name'),
+        # }
+        serializer = UserSerializer(
+                user_query,
+                data=request.data,
+                partial=True,  # not working
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, user_id, *args, **kwargs):
+        user_query = self.getObjectById(user_id)
+        if not user_query:
+            return Response(
+                {"res", "User with this id does not exist"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user_query.delete()
+        return Response(
+            {"res", "User deleted"},
+            status=status.HTTP_200_OK,
+        )
