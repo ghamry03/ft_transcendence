eb01d8ae4882a18e0b9407b87d9e5fba685cf215
MehrinFirdousi <mehrinfirdousi@gmail.com>
2023-12-22 12:59:55 +0400

    Added game model

--- srcs/game_app/offline_game/models.py
+++ srcs/game_app/offline_game/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
-# Create your models here.
+# GID, start time, end time 
+class Game(models.Model):
+	gid = models.AutoField(primary_key=True)
+	def __str__(self):
+        """String for representing the Model object."""
+        return self.gid
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular genre instance."""
+        return reverse('game-detail', args=[str(self.gid)])
