17b5eb39982157a087c22d6726ad53973cecf86e
Lucas de Alencastro Moreira <lde-alen@student.42abudhabi.ae>
2024-01-19 18:33:12 +0400

    rebasing with new models

--- srcs/game_app/game_app/settings.py
+++ srcs/game_app/game_app/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'offline', # if this doesnt work try offline.apps.Offline_gameConfig
+	'rest_framework',
+	'game_api',
 	'online',
 ]
 
@@ -146,3 +147,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+}
+
--- srcs/game_app/game_app/urls.py
+++ srcs/game_app/game_app/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from game_api import urls as game_urls
 from django.views.generic import RedirectView
 
 # Use static() to add URL mapping to serve static files during development (only)
@@ -26,6 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	path('offline/', include('offline.urls')),
 	path('online/', include('online.urls')),
+	path('users/', include('user_app.urls')),
+	path('game/', include('game_app.urls')),
     # path('', RedirectView.as_view(url='offline/', permanent=True)),
 ]
 
