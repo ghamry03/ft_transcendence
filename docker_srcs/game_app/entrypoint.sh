aa000856e07d1d8fdff023e6f2e72dcaa44ca2c2
MehrinFirdousi <mehrinfirdousi@gmail.com>
2024-01-22 11:13:25 +0400

    Removed unused code from gameapp entrypoint

--- docker_srcs/game_app/entrypoint.sh
+++ docker_srcs/game_app/entrypoint.sh
@@ -6,8 +6,3 @@ python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py runserver 0.0.0.0:2000
 # daphne game_app.wsgi:application --bind 0.0.0.0:2000
-DJANGO_SUPERUSER_USERNAME=testuser \
-DJANGO_SUPERUSER_PASSWORD=testpass \
-DJANGO_SUPERUSER_EMAIL="admin@admin.com" \
-python manage.py createsuperuser --noinput
-python manage.py runserver 0.0.0.0:3000
