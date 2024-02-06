1dbe6d9d3da1ca23f4bc7d8b08607a70bb51f90d
MehrinFirdousi <mehrinfirdousi@gmail.com>
2024-02-06 20:31:31 +0400

    Changed outdated field names

--- srcs/game_app/online/models.py
+++ srcs/game_app/online/models.py
@@ -37,7 +37,7 @@ class Game(models.Model):
 
     def get_absolute_url(self):
         """Returns the url to access a particular game instance."""
-        return reverse('game-detail', args=[str(self.gid)])
+        return reverse('game-detail', args=[str(self.id)])
 
 # this model stores info pertaining to one player in the game and their score
 # we chose to separate PlayerMatch from Game to make fetching the data easier for one player 
@@ -52,4 +52,4 @@ class PlayerMatch(models.Model):
 
     def get_absolute_url(self):
         """Returns the url to access a particular genre instance."""
-        return reverse('game-detail', args=[str(self.matchId)])
+        return reverse('game-detail', args=[str(self.id)])
