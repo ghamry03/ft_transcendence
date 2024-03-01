7cb0c18098495ec635126346aba49bd3cff0592c
Omar <omarelghamry@icloud.com>
2024-03-01 17:21:13 +0400

    init profile

--- srcs/main_app/main_app/constants.py
+++ srcs/main_app/main_app/constants.py
@@ -1,2 +1,2 @@
-AUTH_URL = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-0ddaf921e5492df40a174c01c6e982998ac8a8405a4f53a5066bc62006e749b7&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fauthenticate&response_type=code'
+AUTH_URL = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-35b29394fe8563a2ce4ba29874888befc12c95201bf0967654d73ed6aab8c106&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fauthenticate&response_type=code'
 USER_API_URL = 'http://userapp:3000/'
